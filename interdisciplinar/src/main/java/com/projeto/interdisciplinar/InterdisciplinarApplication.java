8f2713a78e2740cec3739a6ea4ce29bb26595cfa
Zeabalosss <iiitwzin@gmail.com>
2026-09-04 14:52:26 -0300

    Add files via upload

---
+++ interdisciplinar/src/main/java/com/projeto/interdisciplinar/InterdisciplinarApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.interdisciplinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterdisciplinarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterdisciplinarApplication.class, args);
+	}
+
+}
---
+++ interdisciplinar/src/test/java/com/projeto/interdisciplinar/InterdisciplinarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.interdisciplinar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterdisciplinarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
