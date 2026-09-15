524bdb98244f2184774524bcee01754c5cde8e82
Zeabalosss <iiitwzin@gmail.com>
2026-09-15 18:48:29 +0000

    Remove figurinhas e padroniza templates com Bootstrap

--- interdisciplinar/src/main/java/com/projeto/interdisciplinar/repository/FigurinhaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projeto.interdisciplinar.repository;
-
-import com.projeto.interdisciplinar.entity.Figurinha;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FigurinhaRepository extends JpaRepository<Figurinha, Long> {
-}
